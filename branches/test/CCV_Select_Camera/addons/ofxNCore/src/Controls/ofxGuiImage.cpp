9651c51f54d4d4f4d8c6e2ceb7f3c2cf041055c0
baicaibang <baicaibang@da66ed7f-4d6a-8cb4-a557-1474cfe16edc>
2011-06-10 01:14:33 +0000

    Add the ofxGuiImage class for camera frame display.
    git-svn-id: https://ccv-multi-cam.googlecode.com/svn@34 da66ed7f-4d6a-8cb4-a557-1474cfe16edc

---
+++ branches/test/CCV_Select_Camera/addons/ofxNCore/src/Controls/ofxGuiImage.cpp
@@ -0,0 +1,10 @@
+//! ofxGuiImage.cpp
+/*!
+*  
+*
+*  Created by Yishi Guo on 06/10/2011.
+*  Copyright 2011 NUI Group. All rights reserved.
+*
+*/
+
+#include "ofxGuiImage.h"
---
+++ branches/test/CCV_Select_Camera/addons/ofxNCore/src/Controls/ofxGuiImage.h
@@ -0,0 +1,28 @@
+//! ofxGuiImage.h
+/*!
+*  
+*
+*  Created by Yishi Guo on 06/10/2011.
+*  Copyright 2011 NUI Group. All rights reserved.
+*
+*/
+
+// ----------------------------------------------
+
+#ifndef OFX_GUI_IMAGE
+#define OFX_GUI_IMAGE
+
+// ----------------------------------------------
+
+#include "ofxGuiTypes.h"
+#include "ofxGuiObject.h"
+
+// ----------------------------------------------
+
+class ofxGuiImage : public ofxGuiObject {
+
+};
+
+// ----------------------------------------------
+
+#endif
